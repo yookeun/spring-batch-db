c41cf0102a0b2a5c6ecfdd8e96ebb315fde379bc
yookeun <ykkim@andwise.com>
2015-06-22 11:26:42 +0900

    gitignore modified

--- src/main/java/books/BookMain.java
+++ src/main/java/books/BookMain.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
@@ -24,7 +23,8 @@ public class BookMain {
 		ApplicationContext ctx = SpringApplication.run(BookMain.class, args);
 		
 		//입력된 결과 확인부분 (배치랑 상관없음)
-        List<Book> results = ctx.getBean(JdbcTemplate.class).query("SELECT  bookName, bookAuthor, bookPrice FROM book", new RowMapper<Book>() {           
+        List<Book> results = ctx.getBean(JdbcTemplate.class).query("SELECT  bookName, bookAuthor, bookPrice FROM book", new RowMapper<Book>() {          
+        	@Override
             public Book mapRow(ResultSet rs, int row) throws SQLException {
             	Book book = new Book();
             	book.setBookName(rs.getString(1));
